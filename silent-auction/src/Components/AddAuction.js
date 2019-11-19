5226637616854219ed8220e3cda0066325655598
Ana Araujo <actanaaraujo@gmail.com>
2019-11-18 21:41:21 -0800

    Add Auction form

--- silent-auction/src/Components/AddAuction.js
+++ silent-auction/src/Components/AddAuction.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../Utils/api';
+import Items from './AddAuctionCard';
 
 // Lables needed for form
     // 1. Name
@@ -10,11 +11,67 @@ import api from '../Utils/api';
     // 6. image
 
 const AddAuction = (props) => {
+    const [item, setItem] = useState({ title: "", body: "" });
+
+    const handleChanges = e => {
+        console.log(note);
+        setItem({ ...item, [e.target.name]: e.target.value });
+    };
+
+    const submitForm = e => {
+        e.preventDefault();
+        props.addNewNote(item);
+        setItem({ title: "", body: "" });
+    };
+    
     
     return (
-        <div>
-            {/* Add form here */}
-        </div>
+        <form onSubmit={submitForm}>
+            <label> Add Item Form </label>
+            <input
+                id="name"
+                placeholder="Name of the item"
+                onChange={handleChanges}
+                name="fname"
+                value={item.title}
+            />
+
+            <label> Starting Price </label>
+            <input
+                id="startingPrice"
+                placeholder="100.00"
+                onChange={handleChanges}
+                name="startingPrice"
+                value={item.startingPrice}
+            />
+
+            <label> Date Starting </label>
+            <input
+                id="startingDate"
+                placeholder="01/01/2020"
+                onChange={handleChanges}
+                name="startingDate"
+                value={item.startingDate}
+            />
+
+            <label> Date Ending </label>
+            <input
+                id="endingDate"
+                placeholder="01/01/2020"
+                onChange={handleChanges}
+                name="endingDate"
+                value={item.endingDate}
+            />
+
+        <   label htmlFor="note">Description</label>
+            <textarea
+                id="note"
+                name="body"
+                onChange={handleChanges}
+                value={item.body}
+            />
+
+        </form>
     )
 }
 
